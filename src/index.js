813839bc4fbde5f689f88efb7bcd93b3b2d3cd2a
Thomas Godwin <leddfoot36@gmail.com>
2021-04-29 19:56:25 +0200

    selection among multiple stores added plus cleanup

--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
-import createHTML from 'components/createHTML';
-import { getData, getStoreByName, getallStores } from 'components/requests'
+// import createHTML from 'components/createHTML';
+'use strict'
+import { getStoreByName, getallStores } from 'components/requests'
 import { renderStores, renderStore, renderNoStoresFound} from 'views/createPage'
 
-let chosenStore = {}
+
 let possibleStores = {}
 let entireListOfStores = {}
 
-/////////////////////
-///////////////////
-//PROBLEM SOLVED WEBPACK NOT PICKING UP INDEX.HTML
-//MUST SET A FILENAME PROPERTY IN THE WEBPACK.CONFIG.JS FILE
-//EXAMPLE BELOW
-
-// plugins: [
-//   new HtmlWebpackPlugin({
-//       filename: '/public/index.html',
-
 import 'main.css';
 
-let searchTerm = 'sandvika'
+let searchTerm = 'zz'
 
 getStoreByName(searchTerm)
 .then(data => {
   if (data.length === 1) {
-    chosenStore = data
-    console.log(chosenStore)
-    renderStore(chosenStore)
+    renderStore(data)
   }
   else if (data.length > 1)  {
-    console.log(`there are ${data.length} results. Select one`)
     possibleStores = data
     renderStores(possibleStores)
   } 
@@ -36,27 +24,28 @@ getStoreByName(searchTerm)
     getallStores(searchTerm)
      .then(data => {
       entireListOfStores = data  
-      filterResults(entireListOfStores)
+      const possibleMatches = filterResults(entireListOfStores)
+      console.log(possibleMatches)
       renderNoStoresFound()    
      })
   } 
-});
+})
 
   const filterResults = function (stores, searchTerm){
-    // console.log(stores)
-    // THIS IS BROKEN
-    // return stores.filter(function (store, index) {
+    console.log(stores)
+    return stores.filter(function (store, index) {
       
-    //   const isCityMatch = store.address.city.toLowerCase().includes(searchTerm.toString().toLowerCase())
-    //   const isStoreNameMatch = store.storeName.toLowerCase().includes(searchTerm.toString().toLowerCase())
-    //   const isStreetAddressMatch = store.address.street.toLowerCase().includes(searchTerm.toString().toLowerCase())
-    //   const isPostalCodeMatch = store.address.postalCode.includes(searchTerm)
+      const isCityMatch = store.address.city.toLowerCase().includes(searchTerm.toString().toLowerCase())
+      const isStoreNameMatch = store.storeName.toLowerCase().includes(searchTerm.toString().toLowerCase())
+      const isStreetAddressMatch = store.address.street.toLowerCase().includes(searchTerm.toString().toLowerCase())
+      const isPostalCodeMatch = store.address.postalCode.includes(searchTerm)
       
-    //   return isCityMatch || isStoreNameMatch || isStreetAddressMatch || isPostalCodeMatch
-    // })
+      return isCityMatch || isStoreNameMatch || isStreetAddressMatch || isPostalCodeMatch
+    })
   }
 
-  document.querySelector('button').addEventListener('click', function () {console.log('shitttt')})
+  
+ 
 
 
 
