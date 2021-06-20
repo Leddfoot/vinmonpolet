99a5b079c251a4cdea587062e557a90e00c6cb48
Thomas Godwin <leddfoot36@gmail.com>
2021-06-20 16:18:15 +0200

    can now make a store a "home store"

--- src/components/preferenceStorage.js
+++ src/components/preferenceStorage.js
@@ -1,30 +1,18 @@
-// let preferredStores = {
-//     //Note: All store names in the vinmonopolet API are unique! && You can only query by store name, not store id
-//     initialize: function () {
-//         if (localStorage) {
-//             // localStorage.clear()
-//             // this.setHomeStore('holmen')
-//             // let homeStore = this.getHomeStore()
-            
-//             // homeStore.trim()
-//             // console.log('homeStore: ', homeStore);
-//             // let z = homeStore.replace(',', '').replace('&', '').toLowerCase().trim()
-//             // console.log('z: ', z);
-//             // return z
-        
-//             // let z = homeStore.split('')
-//             // let a = z.toString('')
-//             // console.log('a: ', a);
-            
-            
-//         }
-//     }, getHomeStore: function () {
-//         return localStorage.getItem("homeStoreName") || 'none set yet'
-//     }, setHomeStore: function (storeName) {        
-//         localStorage.setItem("homeStoreName", storeName)
-//     }
+let preferredStore = {
+    //Note: All store names in the vinmonopolet API are unique! && You can only query by store name, not store id
+    initialize: function () {
+        if (localStorage) {
+            // localStorage.clear()
+            // this.setHomeStore('holmen')
+            return this.getHomeStore()  
+        }
+    }, getHomeStore: function () {
+        return localStorage.getItem("homeStoreName") || 'none set'
+    }, setHomeStore: function (storeName) {     
+        localStorage.setItem("homeStoreName", storeName)
+    }
 
-// }
+}
 
 
-// export { preferredStores }
+export { preferredStore }
